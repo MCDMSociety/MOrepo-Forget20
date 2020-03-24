da68317a7d68104149328afc4bd7240d0d527687
relund <junk@relund.dk>
2020-03-24 11:56:02 +0100

    Tool edited

--- results/convert/tools.R
+++ results/convert/tools.R
@@ -1,31 +1,57 @@
 ## Tools for converting result output to the right format
 ## Note set working dir to source file location
 
-remotes::install_github("MCDMSociety/MOrepo/misc/R/MOrepoTools")
+# remotes::install_github("MCDMSociety/MOrepo/misc/R/MOrepoTools")
 library(MOrepoTools)
+library(gMOIP)
 library(tidyverse)
 dat <- read_csv("data/stat.csv")
 
-for (i in 1:nrow(dat)) {
-   # must load the correct results here
-   pts <- data.frame(z1 = c(27, 30, 31, 34, 42, 43, 49, 51), z2 = c(56, 53, 36, 33, 30, 25, 23, 9),
-      type = c('se', 'us', 'se', 'us', 'us', 'us', 'us', 'se'))
+idx <- c(2001 , 2002 , 2003 , 2004 , 2009 , 2010 , 2011 , 2012 , 3441 , 3442 , 3443 , 3444 , 3449 , 3450 , 3451 , 3452 , 6321 , 6322 , 6323 , 6324 , 6329 , 6330 , 6331 , 6332 , 7041 , 7042 , 7043 , 7044 , 7049 , 7050 , 7051 , 7052)
+
+
+# all result files are in csv format with comma delimitor and dot as decimal mark
+resFiles <- list.files(recursive = T)
+# for (i in idx) {  # 1:nrow(dat)
+   # # must load the correct results here
+   # pts <- data.frame(z1 = c(27, 30, 31, 34, 42, 43, 49, 51), z2 = c(56, 53, 36, 33, 30, 25, 23, 9),
+   #    type = c('se', 'us', 'se', 'us', 'us', 'us', 'us', 'se'))
+
+
+   i = 2001
+   iName <- dat$instance[i]
+   # iName <- "KP_10_3_2box_1_10_1"
+   # if (length(grep(iName, resFiles, value = T)) >0 ) cat(i,", ")
+
+   # fs <- grep(iName, resFiles, value = T)
+   # file.show(fs, pager = "RStudio")
+
+
+   pts <- read_csv(grep(str_c(iName,"_UB.csv"), resFiles, value = T))[,1:dat$p[i]]
+   pts <- addNDSet(pts) %>% select(-(nd:us), type = "cls")
+   if (nrow(pts) != dat$YN[i]) stop("Different number of nondominated points!")
+
+   coeff <- read_csv(grep(str_c(iName,"_coef.csv"), resFiles, value = T))
+   coeffRatio <- sum(coeff$nondominated)/nrow(coeff)
 
    createResultFile(
       instanceName = dat$instance[i],
       contributionName = "Forget20",
       objectives = dat$p[i],
       objectiveType = rep("int", dat$p[i]),
-      direction = rep("min", dat$p[i]),  # are we always using min?
-      cpu = list(sec = 3, machineSpec = "Intel ...")
+      direction = rep("min", dat$p[i]),
+      cpu = c(sec = 3, machineSpec = "Intel ..."),
       points = pts,
       card = dat$YN[i],
-      suppCard = NA,
-      extCard = NA,
+      suppCard = nrow(dplyr::filter(pts, type == "se" | type == "sne")),
+      extCard = nrow(dplyr::filter(pts, type == "us")),
       comments = "",
-      misc = list(instanceStat = list(a=2), treeStat = list(b=3)),  # we add all other thing here
-      optimal = TRUE
+      optimal = TRUE,
+      # we add all other things under misc
+      misc = list(inputStat = list(n = dat$n[i], coeffRatio = coeffRatio),
+                  outputStat = list(),
+                  xE = read_csv(grep(str_c(iName,"_XE.csv"), resFiles, value = T)))
    )
-}
+# }
 
 
