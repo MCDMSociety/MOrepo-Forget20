69cbb2d8a91828db25165089045afb23c779bf82
relund <junk@relund.dk>
2020-05-25 16:05:23 +0200

    Update instance result report

--- results/report/main_report.R
+++ results/report/main_report.R
@@ -8,40 +8,33 @@ loadPackages <- function(packages) {
 loadPackages(c("tidyverse", "rmarkdown"))
 # options(rgl.useNULL=TRUE)
 
-
 oldDir <- setwd("./results/report")
-## Generate result files for some special instances
+## Some sepecial instances
 tmp <- read_csv("../statistics.csv") %>%
    mutate(YNsRatio = YNs/YN, YNusRatio = 1-YNs/YN, YNsneRatio = (YNs-YNse)/YN) %>%
    group_by(instance) %>%
    summarise_at(vars(contains(c("YN", "total"))), list(mean = mean, sd = sd, max = max), na.rm = TRUE)
 inst <- bind_rows(
    tmp %>% top_n(-3, YN_mean),
-   tmp %>% top_n(3, YN_mean),
    tmp %>% top_n(-3, YNusRatio_mean),
    tmp %>% top_n(3, YNusRatio_mean),
    tmp %>% top_n(3, YNsneRatio_mean),
-   tmp %>% top_n(3, tpstotal_sd)
+   tmp %>% top_n(3, tpstotal_sd),
+   tmp %>% top_n(3, YN_mean)
 ) %>% pull(instance)
-for(i in inst){
-   cat("File", i, "\n")
-   if (file.exists(paste0("instances/", i, ".html"))) next
-   rmarkdown::render("instance.Rmd", output_file=paste0("instances/", i, ".html"),
-                     params=list(new_title=paste("Results for instance", i) , currentInstance = i) )
-}
-
-## Generate result file for each instance with 1_2
-inst <- read_csv("../statistics.csv") %>%
+tmp <- read_csv("../statistics.csv") %>%
    dplyr::filter(grepl("1_2", .data$instance)) %>%
    distinct(instance) %>%
    pull(instance)
+inst <- c(inst, tmp)
+inst <- unique(inst)
+# Generate instance reports
 for(i in inst){
   cat("File", i, "\n")
   if (file.exists(paste0("instances/", i, ".html"))) next
   rmarkdown::render("instance.Rmd", output_file=paste0("instances/", i, ".html"),
          params=list(new_title=paste("Results for instance", i) , currentInstance = i) )
 }
-
 setwd(oldDir)
 
 ## Generate result report
