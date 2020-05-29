da61310045e9851e8fc9ebe275a74edf3ebbc7a5
Lars Relund Nielsen <au15463@uni.au.dk>
2020-05-29 13:09:23 +0200

    Updated result files

--- results/report/main_report.R
+++ results/report/main_report.R
@@ -9,34 +9,34 @@ loadPackages(c("tidyverse", "rmarkdown", "fs"))
 # options(rgl.useNULL=TRUE)
 
 oldDir <- setwd("./results/report")
-## Some special instances
-# tmp <- read_csv("../statistics.csv") %>%
-#    mutate(YNsRatio = YNs/YN, YNusRatio = 1-YNs/YN, YNsneRatio = (YNs-YNse)/YN) %>%
-#    group_by(instance) %>%
-#    summarise_at(vars(contains(c("YN", "total"))), list(mean = mean, sd = sd, max = max), na.rm = TRUE)
-# inst <- bind_rows(
-#    tmp %>% top_n(-3, YN_mean),
-#    tmp %>% top_n(-3, YNusRatio_mean),
-#    tmp %>% top_n(3, YNusRatio_mean),
-#    tmp %>% top_n(3, YNsneRatio_mean),
-#    tmp %>% top_n(3, tpstotal_sd),
-#    tmp %>% top_n(3, YN_mean)
-# ) %>% pull(instance)
-# tmp <- read_csv("../statistics.csv") %>%
-#    dplyr::filter(grepl("1_2", .data$instance)) %>%
-#    distinct(instance) %>%
-#    pull(instance)
-# inst <- c(inst, tmp)
-# inst <- unique(inst)
-# # Generate instance reports
-# reset <- TRUE
-# for(i in inst){
-#   cat("File", i, "\n")
-#   if (file.exists(paste0("../../docs/instances/", i, ".html")) & !reset) next
-#   try(rmarkdown::render("instance.Rmd", output_file = paste0(i, ".html"),
-#                     output_dir = "../../docs/instances", quiet = T, #envir = new.env(),
-#          params=list(new_title=paste("Results for instance", i) , currentInstance = i) ))
-# }
+# Some special instances
+tmp <- read_csv("../statistics.csv") %>%
+   mutate(YNsRatio = YNs/YN, YNusRatio = 1-YNs/YN, YNsneRatio = (YNs-YNse)/YN) %>%
+   group_by(instance) %>%
+   summarise_at(vars(contains(c("YN", "total"))), list(mean = mean, sd = sd, max = max), na.rm = TRUE)
+inst <- bind_rows(
+   tmp %>% top_n(-3, YN_mean),
+   tmp %>% top_n(-3, YNusRatio_mean),
+   tmp %>% top_n(3, YNusRatio_mean),
+   tmp %>% top_n(3, YNsneRatio_mean),
+   tmp %>% top_n(3, tpstotal_sd),
+   tmp %>% top_n(3, YN_mean)
+) %>% pull(instance)
+tmp <- read_csv("../statistics.csv") %>%
+   dplyr::filter(grepl("1_2", .data$instance)) %>%
+   distinct(instance) %>%
+   pull(instance)
+inst <- c(inst, tmp)
+inst <- unique(inst)
+# Generate instance reports
+reset <- FALSE
+for(i in inst){
+  cat("File", i, "\n")
+  if (file.exists(paste0("../../docs/instances/", i, ".html")) & !reset) next
+  try(rmarkdown::render("instance.Rmd", output_file = paste0(i, ".html"),
+                    output_dir = "../../docs/instances", quiet = T, #envir = new.env(),
+         params=list(new_title=paste("Results for instance", i) , currentInstance = i) ))
+}
 
 ## Generate result report
 rmarkdown::render("report.Rmd", output_file="report.html", output_dir = "../../docs/")
