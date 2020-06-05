6896a82b3c32e0d09170b2e2ec40fe9fb69b1f65
relund <junk@relund.dk>
2020-06-05 11:20:10 +0200

    Updated report

--- results/report/main_report.R
+++ results/report/main_report.R
@@ -10,20 +10,43 @@ loadPackages(c("tidyverse", "rmarkdown", "fs"))
 
 oldDir <- setwd("./results/report")
 # Some special instances
-tmp <- read_csv("../statistics.csv") %>%
-   mutate(YNsRatio = YNs/YN, YNusRatio = 1-YNs/YN, YNsneRatio = (YNs-YNse)/YN) %>%
+limSec <- c(0, 30*60)  # computation time limits (exclude instances with max cpu < limSec[1] or  min cpu > limSec[2])
+datAll <- read_csv("../statistics.csv") %>%   #"../convert/data/stat.csv"
+   mutate(YNsRatio = YNs/YN,
+          YNusRatio = 1-YNs/YN,
+          YNsneRatio = (YNs-YNse)/YN,
+          algConfig = tolower(str_c(nodesel, varsel, OB, sep="_")),
+          resultName = str_c(instance, algConfig, sep="_"))
+algConfigs <- length(unique(datAll$algConfig))
+tmp <- datAll %>%
    group_by(instance) %>%
-   summarise_at(vars(contains(c("YN", "total"))), list(mean = mean, sd = sd, max = max), na.rm = TRUE)
+   summarise(minCpu = min(tpstotal), maxCpu = max(tpstotal)) %>%
+   filter(maxCpu < limSec[1] | minCpu > limSec[2]) %>%
+   pull(instance)
+
+datAll <- datAll %>%
+   filter(!(instance %in% tmp)) %>%
+   mutate(solved = if_else(tpstotal >= limSec[2] | solved == 0, 0, 1)) %>%
+   group_by(instance) %>%
+   filter(n() == algConfigs) %>%
+   ungroup() %>%
+   mutate(tpstotal = if_else(tpstotal >= limSec[2], limSec[2], tpstotal))
+datNotSolved <- datAll %>% filter(solved == 0)
+datSolved <- datAll %>% filter(solved == 1)
+tmp <- datAll %>%
+   group_by(instance) %>%
+   summarise_at(vars(contains(c("YN", "total", "solved"))), list(mean = mean, sd = sd, max = max), na.rm = TRUE)
 inst <- bind_rows(
-   tmp %>% top_n(-3, YN_mean),
-   tmp %>% top_n(-3, YNusRatio_mean),
-   tmp %>% top_n(3, YNusRatio_mean),
-   tmp %>% top_n(3, YNsneRatio_mean),
-   tmp %>% top_n(3, tpstotal_sd),
-   tmp %>% top_n(3, YN_mean)
+   tmp %>% top_n(-3, YN_max),
+   tmp %>% filter(solved_max == 1) %>% top_n(3, YN_max),
+   tmp %>% filter(solved_max < 1) %>% top_n(3, YN_max),
+   tmp %>% top_n(-3, YNusRatio_max),
+   tmp %>% top_n(3, YNusRatio_max),
+   tmp %>% top_n(3, YNsneRatio_max),
+   tmp %>% top_n(3, tpstotal_sd)
 ) %>% pull(instance)
 tmp <- read_csv("../statistics.csv") %>%
-   dplyr::filter(grepl("1_2", .data$instance)) %>%
+   dplyr::filter(grepl("_1_2", .data$instance)) %>%
    distinct(instance) %>%
    pull(instance)
 inst <- c(inst, tmp)
